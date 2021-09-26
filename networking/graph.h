a0aca0d3bf239274852038331cf2dd532085f305
Victor Olin <victorvi@student.chalmers.se>
2021-09-26 19:21:22 +0200

    Basic file reading for input, changed graph struct

--- networking/graph.h
+++ networking/graph.h
@@ -1,17 +1,26 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <stdint.h>
+
+#define INFINITY    ((uint8_t) 0xFF)
+
 typedef struct {
-    char src;
     char dst;
     uint8_t cost;
 } link;
 
 typedef struct {
-    char *nodes;
+    char c;
+    char link_count;
     link *links;
+} node;
+
+typedef struct {
+    char node_count;
+    node *nodes;
 } graph;
 
-void graph_create(graph *g, int nodes);
+void graph_init(graph *g, int nodes);
 
 #endif
